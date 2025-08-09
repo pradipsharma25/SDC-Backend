6c873a4d53ff2a30f462a87823c9b82f9f5b2319
Pradip Sharma <pradip.sharma@dc.dmn>
2025-08-09 20:46:59 +0545

    successful message

--- src/main/java/com/api/demo/backend/RestApi.java
+++ src/main/java/com/api/demo/backend/RestApi.java
@@ -1,8 +1,29 @@
 package com.api.demo.backend;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.api.demo.model.Test;
+import com.api.demo.repository.TestRepository;
 
 @RestController
 public class RestApi {
+	
+	
+	@Autowired
+	private TestRepository tRepo;
+	
+	@GetMapping("/api/test")
+	public List<Test> getApi() {
+		return tRepo.findAll();
+	}
+	
+	@GetMapping("/api/tests")
+    public String testApi() {
+        return "API is working!";
+    }
 	
 }
